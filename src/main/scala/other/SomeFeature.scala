354516cedbbb80ab53535b0e640e65397db413cb
yc-zhang <turalyon.zhangyc@gmail.com>
2016-12-18 17:39:34 +0800

    Changed after review....

--- src/main/scala/other/SomeFeature.scala
+++ src/main/scala/other/SomeFeature.scala
@@ -4,4 +4,7 @@ object SomeFeature {
   def main(args: Array[String]): Unit = {
     println("I've finished some features of this job.")
   }
+
+  def saySomething() =
+    println("I've said this before...")
 }
